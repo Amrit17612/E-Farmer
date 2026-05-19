31d0572c4d06c859b6804c876b38f25fab850e96
Amrit Raj <amrit17612@gmail.com>
2026-05-20 01:56:17 +0530

    Prevent homepage crash when market data is unavailable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
     {
         // Share market prices on landing page
         view()->composer('welcome', function ($view) {
-            $marketPrices = \App\Models\MarketPrice::latest('price_date')->take(8)->get();
+            try {
+                $marketPrices = \App\Models\MarketPrice::latest('price_date')->take(8)->get();
+            } catch (\Throwable $exception) {
+                report($exception);
+                $marketPrices = new Collection();
+            }
+
             $view->with('marketPrices', $marketPrices);
         });
     }
